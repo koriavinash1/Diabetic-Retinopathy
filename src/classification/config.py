e5d493d80610bd80000fe3062ba0a04cad00e3df
koriavinash1 <koriavinash1@gmail.com>
2018-02-09 18:09:39 +0530

    keras dataloader

--- src/classification/config.py
+++ src/classification/config.py
@@ -7,6 +7,7 @@ class conf(object):
 		self.log_path = '../../logs'
 		self.image_data_path = '../../raw_data/Training Set/'
 		self.label_path = '../../raw_data/IDRiD_Training Set.csv'
+		self.model = 'DR' # can be DR and DME
 
 		self.resize_to = (224, 224)
 		self.resampler_choice = cv2.INTER_AREA # bilinear interpolation
@@ -14,10 +15,17 @@ class conf(object):
 		self.data_augmentation = True
 		self.input_shape = (224, 224, 3)
 		self.resume_training = False
-		self.batch_size = 5
+		self.batch_size = 2
+		self.nclasses = 5
 		self.epochs = 50
 		self.validation_split = 0.05 
 		self.learning_rate = 1e-3
 
 		# Set Environment
 		os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+		if not os.path.exists(self.log_path):
+			os.mkdir(self.log_path)
+
+		if not os.path.exists(self.save_model_path):
+			os.mkdir(self.save_model_path)
